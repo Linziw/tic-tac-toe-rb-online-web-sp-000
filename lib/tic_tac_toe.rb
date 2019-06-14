c9c9fb8fec14b659c1d8aa642cc34f9f4cdfb170
Lindsey Wells <lindsey.wells84@gmail.com>
2019-06-14 10:10:31 +0000

    Automatically backed up by Learn

--- lib/tic_tac_toe.rb
+++ lib/tic_tac_toe.rb
@@ -95,7 +95,7 @@ def play(board)
  turn(board)
 end
  if won?(board)
- puts "Congratulations #{current_player(board)}!"
+ puts "Congratulations #{winner(board}!"
 else if draw?(board)
   puts "Cat's Game!"
 end
